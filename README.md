2fcb186aba4cbbeac8f34429e9203bd1dd994396
Tim Novis <tim.novis@me.com>
2018-01-10 10:40:38 +0000

    Version 0.3.0 - Added a couple of methods and improved matching patterns

--- README.md
+++ README.md
@@ -12,6 +12,13 @@ npm install embed-detector
 import EmbedDetector from 'embed-detector';
 
 let iFrameUrl = EmbedDetector.detect('https://www.youtube.com/watch?v=mKq2nlV4DYk');
+// Returns the correct URL for iFrame
+
+let linkSource = EmbedDetector.getSourceType('https://www.youtube.com/watch?v=mKq2nlV4DYk');
+// Returns 'youtube'
+
+let isMediaLink = EmbedDetector.qualifySource('https://www.youtube.com/watch?v=mKq2nlV4DYk');
+// Returns true
 ```
 
 This package can also be used in the browser with a regular `<script>` tag.
--- src/index.js
+++ src/index.js
@@ -1,36 +1,62 @@
 export default class EmbedDetector {
-  static get pattern() {
-    return /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?([\w\-]{10,12})(?:&feature)?(?:[\w\-]{0})?|(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?|(http|https)?:\/\/(www\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|)(\d+)(?:|\/\?)/;
-  }
   static get sources() {
     return [
       {
         type: 'youtube',
         url: 'http://www.youtube.com/embed/$1',
+        pattern: /^(?:https?:\/\/)?(?:i\.|www\.|img\.)?(?:youtu\.be\/|youtube\.com\/|ytimg\.com\/)(?:embed\/|v\/|vi\/|vi_webp\/|watch\?v=|watch\?.+&v=)((\w|-){11})(?:\S+)?$/,
         matchIndex: 1,
       },
       {
         type: 'vimeo',
         url:
           'https://player.vimeo.com/video/$1?color=f4d807&portrait=0&badge=0',
-        matchIndex: 7,
+        pattern: /https?:\/\/(?:vimeo\.com\/|player\.vimeo\.com\/)(?:video\/|(?:channels\/staffpicks\/|channels\/)|)((\w|-){7,9})/,
+        matchIndex: 1,
       },
       {
         type: 'soundcloud',
         url:
           'https://w.soundcloud.com/player/?url=$1&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=true&amp;show_comments=false&amp;show_user=true&amp;show_reposts=true&amp;show_teaser=true&amp;visual=true',
+        pattern: /https?:\/\/(?:w\.|www\.|)(?:soundcloud\.com\/)(?:(?:player\/\?url=https\%3A\/\/api.soundcloud.com\/tracks\/)|)(((\w|-)[^A-z]{7})|([A-Za-z0-9]+(?:[-_][A-Za-z0-9]+)*(?!\/sets(?:\/|$))(?:\/[A-Za-z0-9]+(?:[-_][A-Za-z0-9]+)*){1,2}))/,
         matchIndex: 0,
       },
     ];
   }
+  static qualifySource(string) {
+    // Test the string against each pattern defined in sources, returns true
+    if (string && string.length > 0) {
+      let isMediaSource = false;
+      EmbedDetector.sources.forEach(source => {
+        if (source.pattern.test(string)) {
+          isMediaSource = true;
+        }
+      });
+      return isMediaSource;
+    }
+  }
+  static getSourceType(string) {
+    // Returns the source type (e.g. 'soundcloud')
+    if (string && string.length > 0) {
+      let sourceType = null;
+      EmbedDetector.sources.forEach(source => {
+        if (source.pattern.test(string)) {
+          sourceType = source.type;
+        }
+      });
+      return sourceType;
+    }
+  }
   static detect(string) {
-    if (string && string.length > 0 && EmbedDetector.pattern.test(string)) {
+    // Return the iFrame URL for string
+    if (string && string.length > 0) {
       let url = null;
       EmbedDetector.sources.forEach(source => {
-        if (string.indexOf(source.type) > -1) {
+        if (source.pattern.test(string) && string.indexOf(source.type) > -1) {
+          console.log(string.match(source.pattern));
           url = source.url.replace(
             '$1',
-            string.match(EmbedDetector.pattern)[source.matchIndex],
+            string.match(source.pattern)[source.matchIndex],
           );
         }
       });
